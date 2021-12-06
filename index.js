b5203ca41ac3ec65e62e6bc14692b986d268b554
valnicky <valery0007@gmail.com>
2021-12-06 16:19:28 +0100

    cnge

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const axios = require('axios');
+const fs = require('fs').promises;
+const path = require('path');
 
-const main = () => {
-    
+const main = async() => {
+    let response = await axios.get('https://rickandmortyapi.com/api/character');
+    let { data: { results } } = response;
+
+    let characters = results.map((character) => {
+        return {
+            id: character.id,
+            name: character.name,
+            status: character.status,
+            species: character.species,
+            image: character.image
+        };
+    }).map((personaje) =>
+        Object.values(personaje).join(',').join('\n'))
+
+    await fs.writeFile(path.join(__dirname, 'data.csv'), characters);
+    console.log(characters);
 }
+
+main();
